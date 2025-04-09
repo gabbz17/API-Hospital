8350f572638207de60adf92d52040cc38c215351
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-04-09 19:17:24 -0300

    concertando a entidade de medico

--- src/main/java/com/example/API_Hospital/entity/Medico.java
+++ src/main/java/com/example/API_Hospital/entity/Medico.java
@@ -5,6 +5,7 @@ import com.example.API_Hospital.entity.Role.Pessoa;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,7 +19,8 @@ import java.math.BigDecimal;
 public class Medico extends Pessoa {
 
     @NotBlank
-    @Column(unique = true, length = 11)
+    @Column(unique = true)
+    @Size(min = 11, max = 11)
     private String cpf;
     @NotBlank
     @Column(unique = true)
